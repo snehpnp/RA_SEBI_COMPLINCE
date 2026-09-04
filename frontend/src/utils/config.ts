5880755bf1782fee8df6f5a143ed93bd6cf387d1
Anuj Paterya <anujpaterya29@gmail.com>
2026-09-04 17:04:31 +0530

    Done

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,5 +1,5 @@
-export const base_ra_url = typeof window !== 'undefined' && window.location.hostname === "localhost" 
-  ? "https://compliance.pnpuniverse.in/backend" 
-  : typeof window !== 'undefined' ? `${window.location.origin}/backend` : "https://compliance.pnpuniverse.in/backend";
+export const base_ra_url = typeof window !== 'undefined' && window.location.hostname === "localhost"
+  ? "http://localhost:5000"
+  :`${window.location.origin}/backend`;
 
 export const base_api_url = `${base_ra_url}/api/v1`;
